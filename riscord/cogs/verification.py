2e201c52a62789e4e9ce278781be84dff5b34ee8
noob <siddheshkale007@gmail.com>
2020-09-08 07:01:29 -0700

    Fixed bot perms

--- riscord/cogs/verification.py
+++ riscord/cogs/verification.py
@@ -23,9 +23,14 @@ class Verification(commands.Cog):
 				id_val = "s" + id_val[1:-1]
 
 			if id_val == student_id:
+				member = ctx.message.author
+				print(member.roles)
 				member_role = discord.utils.get(member.guild.roles, name="Member")
+				try:
+					await member.add_roles(member_role)
+				except	:
+					print("Cunt	")
 				await ctx.send("Welcome to the club :tada:")
-				await ctx.author.add_roles(ctx.author, member_role)
 				return True
 
 		await ctx.send("User not found")
